bb940dde4f7862aacc6d0d8cdcb4aa167500d1f2
Alvin Cho <alvinxcho2@gmail.com>
2023-03-23 21:32:49 +0000

    Completed background

--- client/components/hello-world.jsx
+++ client/components/hello-world.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 
 export default function HelloWorld(props) {
   return (
-    <>
-      <img className="valodle-logo" src="./images/valodle.jpg" alt="valodle logo" />
-      <img className="jett-background" src="./images/jett.jpg" />
-    </>
+    <img className="jett-background" src="./images/jett.jpg" />
   );
 }
